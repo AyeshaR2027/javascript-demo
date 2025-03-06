1c744202000d9f2f4822468e699a5b73acf9908b
AyeshaR2027 <ayesha.rehman2027@gbwl.org>
2025-03-06 18:37:07 +0000

    working

--- script.js
+++ script.js
@@ -14,4 +14,25 @@ let pokemon;
 
 // ASSIGN a VALUE to the variable
 // its like putting an item in the box (storing it for later)
-pokemon = "Jiggly-Puff"
+pokemon = "Jiggly-Puff"   
+
+//DECLARE + ASSIGN in one statement 
+
+let luckyNumber = 7;
+
+// "PASS IN" a variable to a FUNCTION
+console.log(luckyNumber);
+
+// CHANGE the value of an existing variable 
+let raining = false;
+console.log(raining);
+raining = true;
+console.log(raining);  
+
+//COPY data from one variable into another 
+let breakfast = "playa bowl coffee";
+let lunch; 
+lunch = breakfast
+console.log(lunch);
+console.log(breakfast);
+// Both varables hold the same value
